6d78bbcc8e51209e6f8a6d4948adde3d8ffc8c65
Daniel Morales <mralejo99@gmail.com>
2023-08-21 15:24:00 -0500

    feat: added validation when create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserValidator } from './validators/createUser.validator';
 import { UserRepository } from './repositories/user.repository';
 
@@ -7,6 +7,16 @@ export class UsersService {
   constructor(private usersRepository: UserRepository) {}
 
   async createUser(user: CreateUserValidator) {
+    const { email } = user;
+    const currentUser = await this.usersRepository.findOne({
+      where: { email },
+    });
+    if (currentUser)
+      throw new BadRequestException('Email is already used', {
+        description: 'A user with this emails exits',
+        cause: new Error(),
+      });
+
     const newUser = this.usersRepository.create(user);
 
     return await this.usersRepository.save(newUser);

76cd8bc2bd82e9c87f717eeb6913e77e2ae56945
Daniel Morales <mralejo99@gmail.com>
2023-08-21 01:17:15 -0500

    feat: added controllers folder

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './users.model';
 import { UserRepository } from './repositories/user.repository';
+import { UsersController } from './controllers/v1/users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users])],
